c8577ae3598ddd6c8df598cde9156f21973072a8
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-14 10:51:53 +0300

    MessageDialog.qml Oluşturuldu. Backend Class Oluşturuldu.

---
+++ Comman/backend.h
@@ -0,0 +1,17 @@
+#ifndef BACKEND_H
+#define BACKEND_H
+
+#include <QObject>
+
+class Backend : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Backend(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // BACKEND_H
