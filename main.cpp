ea23ad59c3c4d48d5d30d695c9d584664a0a9408
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-14 17:41:31 +0300

    User ve DB Nesneleri Eklendi.

--- main.cpp
+++ main.cpp
@@ -3,15 +3,30 @@
 #include <QQmlContext>
 #include "Comman/backend.h"
 
+#include "../url.h"
+#include "mongocxx/instance.hpp"
+
+#include "Base/item.h"
+#include "Base/db.h"
+#include "Base/user.h"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    mongocxx::instance ins{};
+
+    auto db = new DB();
+
+    auto user =new User(db);
+
 
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
 
     engine.rootContext ()->setContextProperty ("Backend",new Backend());
+    engine.rootContext ()->setContextProperty ("User",user);
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
@@ -20,6 +35,7 @@ int main(int argc, char *argv[])
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
     engine.load(url);
+
 
     return app.exec();
 }
