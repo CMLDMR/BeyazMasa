a435bf5c5e05164c4da061e938ccaf425f669ac0
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-14 10:53:43 +0300

    Düzeltildi.

--- ScriptFiles/LoginScreen.js
+++ ScriptFiles/LoginScreen.js
@@ -8,16 +8,16 @@ function loadLoginScreen() {
     var component = Qt.createComponent("qrc:/Login/Login.qml");
 
     if (component.status === Component.Ready) {
-            var sprite = component.createObject(appWindow);
-            if (sprite === null) {
-                // Error Handling
-                console.log("Error creating object");
-            }else{
-                console.log("Creating Object");
-            }
-        } else if (component.status === Component.Error) {
+        var sprite = component.createObject(appWindow);
+        if (sprite === null) {
             // Error Handling
-            console.log("Error loading component:", component.errorString());
+            console.log("Error creating object");
+        }else{
+            console.log("Creating Object");
         }
+    } else if (component.status === Component.Error) {
+        // Error Handling
+        console.log("Error loading component:", component.errorString());
+    }
 
 }
