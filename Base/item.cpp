9af8657b72ab4aa41c73776961b87a481959f56b
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-14 13:47:01 +0300

    Item Base Oluşturuldu.

--- Base/item.cpp
+++ Base/item.cpp
@@ -1,6 +1,47 @@
 #include "item.h"
 
 Item::Item()
+    :mDoc(document{})
+{
+    mDoc.clear ();
+}
+
+void Item::operator=(const bsoncxx::builder::basic::document &value)
+{
+    mDoc.clear ();
+
+    for( auto item : value.view () )
+    {
+        this->append(item.key ().to_string().c_str (),item.get_value ());
+    }
+}
+
+void Item::operator=(const Item &value)
+{
+    mDoc.clear ();
+
+    for( auto item : value.view () )
+    {
+        this->append(item.key ().to_string().c_str (),item.get_value ());
+    }
+}
+
+bsoncxx::document::view Item::view() const
+{
+    return mDoc.view ();
+}
+
+boost::optional<bsoncxx::types::value> Item::element(QString key)
 {
 
+    try {
+        return mDoc.view ()[key.toStdString ()].get_value ();
+    } catch (bsoncxx::exception &e) {
+        std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+        std::cout << str << std::endl;
+        return boost::none;
+    }
+
+
 }
+
--- Base/item.h
+++ Base/item.h
@@ -1,11 +1,39 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#include "mongoheaders.h"
+#include <QString>
+#include <iostream>
+#include <boost/optional.hpp>
 
 class Item
 {
 public:
     Item();
+
+
+
+
+    void operator=(const document &value);
+    void operator=(const Item &value);
+
+    bsoncxx::document::view view() const;
+
+    boost::optional<bsoncxx::types::value> element(QString key);
+
+    template<typename T>
+    void append(QString key ,const T &value){
+        try {
+            mDoc.append (kvp(key.toStdString (),value));
+        } catch (bsoncxx::exception &e) {
+            std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+            std::cout << str << std::endl;
+        }
+    }
+
+
+private:
+    document mDoc;
 };
 
 #endif // ITEM_H
