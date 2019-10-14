9c4821340d25677731a3581ae20a7d0dda5e238d
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-14 10:53:01 +0300

    Message Eklendi ve Signal ve Slot Uygulandı.

--- Comman/backend.cpp
+++ Comman/backend.cpp
@@ -4,3 +4,18 @@ Backend::Backend(QObject *parent) : QObject(parent)
 {
 
 }
+
+QString Backend::message() const
+{
+    return mMessage;
+}
+
+void Backend::setMessage(const QString &message)
+{
+    if( mMessage == message )
+    {
+//        return;
+    }
+    mMessage = message;
+    emit messageChanged (mMessage);
+}
--- Comman/backend.h
+++ Comman/backend.h
@@ -6,12 +6,21 @@
 class Backend : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString message READ message WRITE setMessage NOTIFY messageChanged)
+
 public:
     explicit Backend(QObject *parent = nullptr);
 
-signals:
+    QString message() const;
+    void setMessage(const QString &message);
 
+signals:
+    void messageChanged(QString _message);
 public slots:
+
+
+private:
+    QString mMessage;
 };
 
 #endif // BACKEND_H
