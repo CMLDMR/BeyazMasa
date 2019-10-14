bacf54c39d34b3e99961f3c1efae58b612808f61
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-14 13:47:22 +0300

    Methodlar eklendi.

--- Base/db.cpp
+++ Base/db.cpp
@@ -3,4 +3,31 @@
 DB::DB()
 {
 
+    try {
+        mClient = new mongocxx::client(mongocxx::uri(_url));
+    } catch (mongocxx::exception &e) {
+        std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+        std::cout << str << std::endl;
+    }
+
+    _mDB = mClient->database (DB__);
+
+    this->mDB = &_mDB;
+}
+
+DB::DB(mongocxx::database *_db)
+    :mDB(_db)
+{
+
+}
+
+DB::DB(DB *_db)
+    :mDB(_db->db ())
+{
+
+}
+
+mongocxx::database *DB::db()
+{
+    return this->mDB;
 }
--- Base/db.h
+++ Base/db.h
@@ -1,11 +1,29 @@
 #ifndef DB_H
 #define DB_H
 
+#include "mongoheaders.h"
+#include "../url.h"
+#include <iostream>
+
 
 class DB
 {
 public:
-    DB();
+    explicit DB();
+    DB(mongocxx::database* _db);
+    DB(DB* _db);
+
+    mongocxx::database* db();
+
+private:
+    mongocxx::database* mDB;
+
+
+
+
+private:
+    mongocxx::database _mDB;
+    mongocxx::client* mClient;
 };
 
 #endif // DB_H
