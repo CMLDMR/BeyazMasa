8e9b34095b59c1d79b1671a0af9d9a3aaab78036
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-18 17:41:34 +0300

    TCItem Güncellendi.

--- TC/tcitem.cpp
+++ TC/tcitem.cpp
@@ -1,6 +1,53 @@
 #include "tcitem.h"
 
-TCItem::TCItem(QObject *parent) : QObject(parent)
+TCItem::TCItem(QObject *parent) : QObject(parent) , TC()
 {
 
+}
+
+TCItem::TCItem(const TCItem &other)
+{
+    this->setDocumentView (other.view ());
+}
+
+bool TCItem::tcCheck(const QString &mTC)
+{
+    TC item;
+    item.setTCNO (mTC);
+    auto count = this->countItem (item);
+
+    if( count == -1 )
+    {
+        std::cout << "mTC DataBase Count Error: " << __LINE__ << __FUNCTION__ << std::endl;
+        return false;
+    }else if (count == 0 ) {
+        return false;
+    }else{
+        return true;
+    }
+
+}
+
+bool TCItem::cepTelefonuCheck(const QString &mCepTelefonu)
+{
+    TC item;
+    item.setCepTelefonu (mCepTelefonu);
+    auto count = this->countItem (item);
+
+    if( count == -1 )
+    {
+        std::cout << "mCepTelefonu DataBase Count Error: " << __LINE__ << __FUNCTION__ << std::endl;
+        return false;
+    }else if (count == 0 ) {
+        return false;
+    }else{
+        return true;
+    }
+}
+
+
+
+void TCItem::testView()
+{
+    std::cout << bsoncxx::to_json (this->view ()) << std::endl;
 }
--- TC/tcitem.h
+++ TC/tcitem.h
@@ -2,16 +2,34 @@
 #define TCITEM_H
 
 #include <QObject>
+#include "tc.h"
 
-class TCItem : public QObject
+class TCItem : public QObject , public TC
 {
     Q_OBJECT
+    Q_PROPERTY(QString TCNO READ TCNO WRITE setTCNO)
+    Q_PROPERTY(QString AdSoyad READ AdSoyad WRITE setAdSoyad)
+    Q_PROPERTY(QString CepTelefonu READ CepTelefonu WRITE setCepTelefonu)
+    Q_PROPERTY(QString Mahalle READ Mahalle WRITE setMahalle)
+    Q_PROPERTY(QString TamAdres READ TamAdres WRITE setTamAdress)
+    Q_PROPERTY(QString Password READ Password WRITE setPassword)
+    Q_PROPERTY(QString NormalTelefon READ NormalTelefon WRITE setNormalTelefon)
+    Q_PROPERTY(bool CalismaSMS READ CalismaSMS WRITE setCalismaSMS )
 public:
     explicit TCItem(QObject *parent = nullptr);
+    TCItem( const TCItem &other);
+
+    Q_INVOKABLE bool tcCheck( const QString& mTC );
+    Q_INVOKABLE bool cepTelefonuCheck( const QString& mCepTelefonu );
+
+
+    Q_INVOKABLE void testView();
 
 signals:
 
 public slots:
 };
+
+Q_DECLARE_METATYPE(TCItem)
 
 #endif // TCITEM_H
