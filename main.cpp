9b158178d9c41633136ea9b9ef2e5a4472231eb8
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-18 17:41:45 +0300

    Düzeltme Yapıldı.

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include "db.h"
 #include "Login/personel.h"
 #include "Dilekce/dilekcegirispage.h"
+#include "TC/tcitem.h"
 
 int main(int argc, char *argv[])
 {
@@ -22,17 +23,19 @@ int main(int argc, char *argv[])
     auto user =new Personel();
 
     qmlRegisterType <DilekceGirisPage>("serik.bel.tr.DilekceGirisPage",1,0,"DilekceGirisBackend");
-    qmlRegisterInterface<YeniDilekce>("YeniDilekce");
+    qmlRegisterType <YeniDilekce>("serik.bel.tr.YeniDilekce",1,0,"YeniDilekce");
+    qmlRegisterType <TCItem>("serik.bel.tr.TCItem",1,0,"TCItem");
+
 
     QGuiApplication app(argc, argv);
 
-    app.setOrganizationName("Some Company");
-    app.setOrganizationDomain("somecompany.com");
-    app.setApplicationName("Amazing Application");
+    app.setOrganizationName("Serik Belediyesi");
+    app.setOrganizationDomain("www.serik.bel.tr");
+    app.setApplicationName("Beyaz MASA");
 
     QQmlApplicationEngine engine;
 
-    engine.rootContext ()->setContextProperty ("Backend",new Backend());
+    engine.rootContext ()->setContextProperty ("Backend",new Backend(db));
     engine.rootContext ()->setContextProperty ("User",user);
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
