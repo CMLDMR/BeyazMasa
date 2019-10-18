9d5c1c384f4b9b81f9b253f295f2a634cafc1cb1
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-18 17:40:45 +0300

    loadTCView Eklendi.

--- TC/TCScript.js
+++ TC/TCScript.js
@@ -1,3 +1,23 @@
 function func() {
 
 }
+
+function loadTCView(){
+
+
+    var component = Qt.createComponent("qrc:/TC/TCView.qml");
+
+    if (component.status === Component.Ready) {
+        var sprite = component.createObject(appWindow);
+        if (sprite === null) {
+            // Error Handling
+            console.log("Error creating object");
+        }else{
+
+        }
+    } else if (component.status === Component.Error) {
+        // Error Handling
+        console.log("Error loading component:", component.errorString());
+    }
+
+}
