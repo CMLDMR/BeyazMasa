9bb9e1c565c28d7b0ca4d4f79ac9c2d114a42565
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-22 15:39:35 +0300

    DilekceItem dosyası Düzeltildi.

---
+++ Dilekce/dilekceitem.cpp
@@ -0,0 +1,6 @@
+#include "dilekceitem.h"
+
+DilekceItem::DilekceItem(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ Dilekce/dilekceitem.h
@@ -0,0 +1,17 @@
+#ifndef DILEKCEITEM_H
+#define DILEKCEITEM_H
+
+#include <QObject>
+
+class DilekceItem : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DilekceItem(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // DILEKCEITEM_H
