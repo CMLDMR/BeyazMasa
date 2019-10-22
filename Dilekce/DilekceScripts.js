97d6aacb6ad77a63a75b9d80e4fc9ddf5645f949
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-22 11:25:39 +0300

    loadDilekceListPage function eklendi.

--- Dilekce/DilekceScripts.js
+++ Dilekce/DilekceScripts.js
@@ -46,6 +46,24 @@ function loadYeniDilekcePage(){
 }
 
 
+function loadDilekceListPage()
+{
+    var component = Qt.createComponent("qrc:/Dilekce/DilekceListPage.qml");
+
+    if (component.status === Component.Ready) {
+        var sprite = component.createObject(appWindow);
+        if (sprite === null) {
+            // Error Handling
+            console.log("Error creating object");
+        }else{
+
+        }
+    } else if (component.status === Component.Error) {
+        // Error Handling
+        console.log("Error loading component:", component.errorString());
+    }
+}
+
 
 
 
