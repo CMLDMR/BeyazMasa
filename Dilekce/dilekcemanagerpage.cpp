5d43505431f6d84c166fa58232da1ecb9aaf011a
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-22 15:41:59 +0300

    DilekceManager Sınıfından Miras Alındı.

--- Dilekce/dilekcemanagerpage.cpp
+++ Dilekce/dilekcemanagerpage.cpp
@@ -1,6 +1,123 @@
 #include "dilekcemanagerpage.h"
+#include <QVector>
+#include <QUrl>
 
-DilekceManagerPage::DilekceManagerPage(QObject *parent) : QObject(parent)
+DilekceManagerPage::DilekceManagerPage(QObject *parent) : QObject(parent) , DilekceManager ()
 {
 
+
+
 }
+
+QQmlListProperty<DilekceItem> DilekceManagerPage::dilekceList()
+{
+    return QQmlListProperty<DilekceItem>(this,&mDilekceItemlist,&DilekceManagerPage::appendList , &DilekceManagerPage::listSize , &DilekceManagerPage::cardAt , &DilekceManagerPage::clearListPtr);
+}
+
+void DilekceManagerPage::dilekceListByTelefon(const QString &telefon)
+{
+    this->mDilekceItemlist.clear ();
+    auto cursor = this->findByTelefon (telefon);
+    for( auto doc : cursor )
+    {
+        DilekceItem item;
+        item = doc;
+        this->mDilekceItemlist.append (std::move(item));
+    }
+    emit dilekceListChanged ();
+}
+
+bool DilekceManagerPage::saveDilekce( DilekceItem *_item)
+{
+    auto item = qobject_cast<DilekceItem*>(_item);
+
+    for( auto doc : item->bilgiBirimList ())
+    {
+        item->AddBilgiBirim (doc.toString ());
+    }
+
+    for( auto doc : item->ekList ())
+    {
+        auto fOid = this->uploadfile ( doc.toString ().toStdString ().c_str ());
+        item->AddEkOid (fOid.get_oid ().value.to_string ().c_str ());
+    }
+
+    auto tOid = this->uploadfile (QUrl(item->dilekceFilePath ()).toLocalFile ());
+
+    item->SetDilekceOid (tOid.get_oid ().value.to_string ().c_str () );
+
+
+    return this->insertDilekce (_item);
+}
+
+void DilekceManagerPage::appendList(QQmlListProperty<DilekceItem> *property, DilekceItem *note)
+{
+    Q_UNUSED(property)
+    Q_UNUSED(note)
+}
+
+DilekceItem *DilekceManagerPage::cardAt(QQmlListProperty<DilekceItem> *property, int index)
+{
+    return static_cast<QList<DilekceItem*>*>(property->data)->at(index);
+
+}
+
+int DilekceManagerPage::listSize(QQmlListProperty<DilekceItem> *property)
+{
+    return static_cast<QList<DilekceItem*>*>(property->data)->size();
+
+}
+
+void DilekceManagerPage::clearListPtr(QQmlListProperty<DilekceItem> *property)
+{
+    static_cast<QList<DilekceItem*>*>(property->data)->clear ();
+
+}
+
+
+
+
+
+
+
+
+
+
+//QQmlListProperty<DilekceItem> Storage::getList()
+//{
+//    {
+//        DilekceItem item;
+//        item.SetKonu ("testhh  ");
+//        this->noteItems.append (item);
+//    }
+
+//    {
+//        DilekceItem item;
+//        item.SetKonu ("testhhyyyyyy  ");
+//        this->noteItems.append (item);
+//    }
+
+//    return QQmlListProperty<DilekceItem>(this,&noteItems,&Storage::appendList , &Storage::listSize , &Storage::cardAt , &Storage::clearListPtr);
+//}
+
+//void Storage::appendList(QQmlListProperty<DilekceItem> *property, DilekceItem *note)
+//{
+//    Q_UNUSED(property)
+//    Q_UNUSED(note)
+//}
+
+//DilekceItem *Storage::cardAt(QQmlListProperty<DilekceItem> *property, int index)
+//{
+//    return static_cast<QList<DilekceItem*>*>(property->data)->at(index);
+
+//}
+
+//int Storage::listSize(QQmlListProperty<DilekceItem> *property)
+//{
+//    return static_cast<QList<DilekceItem*>*>(property->data)->size();
+//}
+
+//void Storage::clearListPtr(QQmlListProperty<DilekceItem> *property)
+//{
+//    static_cast<QList<DilekceItem*>*>(property->data)->clear ();
+//}
--- Dilekce/dilekcemanagerpage.h
+++ Dilekce/dilekcemanagerpage.h
@@ -3,16 +3,60 @@
 
 #include <QObject>
 #include "dilekcemanager.h"
+#include "dilekceitem.h"
+#include <QQmlListProperty>
+
 
 class DilekceManagerPage : public QObject , public DilekceManager
 {
     Q_OBJECT
+    Q_PROPERTY(QQmlListProperty<DilekceItem> dilekceList READ dilekceList NOTIFY dilekceListChanged )
+
 public:
     explicit DilekceManagerPage(QObject *parent = nullptr);
 
+    QQmlListProperty<DilekceItem> dilekceList();
+    Q_INVOKABLE void dilekceListByTelefon( const QString &telefon);
+    QList<DilekceItem> mDilekceItemlist;
+
+
+
+
+
+    Q_INVOKABLE bool saveDilekce(DilekceItem *_item );
+
+
+
 signals:
 
+    void dilekceListChanged();
+
+
 public slots:
+
+private:
+    static void appendList(QQmlListProperty<DilekceItem> *property, DilekceItem *note);
+    static DilekceItem* cardAt(QQmlListProperty<DilekceItem> *property, int index);
+    static int listSize(QQmlListProperty<DilekceItem> *property);
+    static void clearListPtr(QQmlListProperty<DilekceItem> *property);
 };
+
+
+
+
+//class Storage : public QObject , public DilekceManager
+//{
+//    Q_OBJECT
+//    Q_PROPERTY(QQmlListProperty<DilekceItem> getList READ getList)
+
+//public:
+//    QQmlListProperty<DilekceItem> getList();
+//    QList<DilekceItem> noteItems;
+//private:
+//    static void appendList(QQmlListProperty<DilekceItem> *property, DilekceItem *note);
+//    static DilekceItem* cardAt(QQmlListProperty<DilekceItem> *property, int index);
+//    static int listSize(QQmlListProperty<DilekceItem> *property);
+//    static void clearListPtr(QQmlListProperty<DilekceItem> *property);
+//};
 
 #endif // DILEKCEMANAGERPAGE_H
