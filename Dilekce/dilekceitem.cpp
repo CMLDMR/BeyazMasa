8415ba5c3b4c21bb3f16e0e259522e23c8f49430
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-22 15:41:36 +0300

    Tüm Özellikleri Tanımlandı.

--- Dilekce/dilekceitem.cpp
+++ Dilekce/dilekceitem.cpp
@@ -1,6 +1,121 @@
 #include "dilekceitem.h"
+#include <boost/optional.hpp>
+#include <QUrl>
 
-DilekceItem::DilekceItem(QObject *parent) : QObject(parent)
+DilekceItem::DilekceItem(QObject *parent) : QObject(parent) , Dilekce ()
 {
+    this->SetSayi (-1);
+}
 
+DilekceItem::DilekceItem(const DilekceItem &dilekce)
+{
+    std::cout << "copy Cosntructor" << std::endl;
+    this->setDocumentView (dilekce.view ());
+}
+
+DilekceItem::DilekceItem(const DilekceItem &&dilekce)
+{
+    this->setDocumentView (dilekce.view ());
+}
+
+DilekceItem::~DilekceItem()
+{
+    std::cout << "Destrutor of ~DilekceItem" << std::endl;
+}
+
+void DilekceItem::operator=(const Dilekce &dilekce)
+{
+    this->setDocumentView (dilekce.view ());
+}
+
+void DilekceItem::operator=(Dilekce *dilekce)
+{
+    this->setDocumentView (dilekce->view ());
+}
+
+
+QJsonArray DilekceItem::ekList() const
+{
+    return mEkList;
+}
+
+void DilekceItem::addEk(const QString &ekPath)
+{
+    if( !mEkList.contains (ekPath) )
+    {
+        mEkList.append (QUrl(ekPath).toLocalFile ());
+        emit ekListChanged ();
+    }
+
+}
+
+bool DilekceItem::deleteEk(const QString &ekPath)
+{
+    int i = -1;
+    bool exist = false;
+    for( auto item : mEkList )
+    {
+        i++;
+        if( item == ekPath ){
+            exist = true;
+            break;
+        }
+
+    }
+    if( i != -1 && exist )
+    {
+        mEkList.removeAt (i);
+        emit ekListChanged ();
+        return true;
+    }
+
+    return false;
+}
+
+QJsonArray DilekceItem::bilgiBirimList() const
+{
+    return mBilgiBirimList;
+}
+
+void DilekceItem::addBilgiBirim(const QString &_birim)
+{
+    if( !mBilgiBirimList.contains (_birim) )
+    {
+        mBilgiBirimList.append (_birim);
+        emit bilgiBirimChanged ();
+    }
+
+}
+
+bool DilekceItem::deleteBilgiBirim(const QString &_birim)
+{
+    int i = -1;
+    bool exist = false;
+    for( auto item : mBilgiBirimList )
+    {
+        i++;
+        if( item == _birim ){
+            exist = true;
+            break;
+        }
+
+    }
+    if( i != -1 && exist )
+    {
+        mBilgiBirimList.removeAt (i);
+        emit bilgiBirimChanged ();
+        return true;
+    }
+
+    return false;
+}
+
+QString DilekceItem::dilekceFilePath() const
+{
+    return mDilekceFilePath;
+}
+
+void DilekceItem::setDilekceFilePath(const QString &dilekceFilePath)
+{
+    mDilekceFilePath = dilekceFilePath;
 }
--- Dilekce/dilekceitem.h
+++ Dilekce/dilekceitem.h
@@ -1,17 +1,67 @@
 #ifndef DILEKCEITEM_H
 #define DILEKCEITEM_H
 
-#include <QObject>
 
-class DilekceItem : public QObject
+#include <QObject>
+#include "dilekce.h"
+#include <QJsonArray>
+
+class DilekceItem : public QObject , public Dilekce
 {
     Q_OBJECT
+    Q_PROPERTY (int sayi READ sayi WRITE SetSayi)
+    Q_PROPERTY (QString konu READ konu WRITE SetKonu)
+    Q_PROPERTY (QString tcoid READ tcoid WRITE SetTCOid )
+    Q_PROPERTY (QString birim READ birim WRITE SetBirim )
+    Q_PROPERTY (QString icerikTipi READ icerikTipi WRITE SetIcerikTipi )
+    Q_PROPERTY (QString icerik READ icerik WRITE SetIcerik )
+    Q_PROPERTY (int tarihJulianDay READ tarihJulian WRITE SetTarih )
+    Q_PROPERTY (QString tarihText READ tarihText )
+    Q_PROPERTY (int saatMSecStartofDay READ saatMSecStartofDay WRITE SetSaat )
+    Q_PROPERTY (QString saatText READ saatText )
+    Q_PROPERTY (QJsonArray ekList READ ekList NOTIFY ekListChanged )
+    Q_PROPERTY (QJsonArray bilgiBirim READ bilgiBirimList NOTIFY bilgiBirimChanged )
+    Q_PROPERTY (QString dilekceFilePath READ dilekceFilePath WRITE setDilekceFilePath )
 public:
     explicit DilekceItem(QObject *parent = nullptr);
+    DilekceItem(const DilekceItem& dilekce);
+    DilekceItem(const DilekceItem&& dilekce);
+    ~DilekceItem();
+
+    void operator=( const Dilekce &dilekce);
+    void operator=( Dilekce* dilekce);
+
+
+
+
+
+
+    Q_INVOKABLE QJsonArray ekList() const;
+    Q_INVOKABLE void addEk( const QString &ekPath );
+    Q_INVOKABLE bool deleteEk( const QString &ekPath );
+
+    Q_INVOKABLE QJsonArray bilgiBirimList() const;
+    Q_INVOKABLE void addBilgiBirim( const QString &_birim );
+    Q_INVOKABLE bool deleteBilgiBirim( const QString &_birim );
+
+    QString dilekceFilePath() const;
+    void setDilekceFilePath(const QString &dilekceFilePath);
 
 signals:
+    void ekListChanged();
+    void bilgiBirimChanged();
 
 public slots:
+
+private:
+
+    QJsonArray mEkList;
+    QJsonArray mBilgiBirimList;
+
+    QString mDilekceFilePath;
+
 };
+
+Q_DECLARE_METATYPE(DilekceItem)
 
 #endif // DILEKCEITEM_H
