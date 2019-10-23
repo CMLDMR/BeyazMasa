e8d1897bd05087540d44aa671fa6d035a465e115
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-23 09:59:55 +0300

    DataBase Register Edildi.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include "Comman/backend.h"
+#include "Comman/database.h"
 
 #include "../url.h"
 #include "mongocxx/instance.hpp"
@@ -20,17 +21,19 @@ int main(int argc, char *argv[])
 
     mongocxx::instance ins{};
 
-    auto db = new DB();
-
+    auto db = new DataBase();
+    auto backend = new Backend(db);
     auto user =new Personel();
 
+    qmlRegisterType<DataBase>();
     qmlRegisterType <DilekceGirisPage>("serik.bel.tr.DilekceGirisPage",1,0,"DilekceGirisBackend");
     qmlRegisterType <DilekceItem>("serik.bel.tr.DilekceItem",1,0,"DilekceItem");
     qmlRegisterType <TCItem>("serik.bel.tr.TCItem",1,0,"TCItem");
     qmlRegisterType <DilekceManagerPage>("serik.bel.tr.DilekceManagerPage",1,0,"DilekceManagerPage");
 
-//    qmlRegisterType<DilekceItem>();
 //    qmlRegisterInterface<QList<DilekceItem*>>("DilekceItemList");
+
+
 
     QGuiApplication app(argc, argv);
 
@@ -40,8 +43,9 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
 
-    engine.rootContext ()->setContextProperty ("Backend",new Backend(db));
+    engine.rootContext ()->setContextProperty ("Backend",backend);
     engine.rootContext ()->setContextProperty ("User",user);
+    engine.rootContext ()->setContextProperty ("DB",db);
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
