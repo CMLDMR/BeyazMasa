c7ba26644b7cacc1d05c2a4a5efbbb5844ec32af
Cemal DEMİR <cemaldemir07@gmail.com>
2019-10-30 13:56:10 +0300

    TCManagerPage Oluşturuldu.

--- TC/tcmanagerpage.cpp
+++ TC/tcmanagerpage.cpp
@@ -1,6 +1,56 @@
 #include "tcmanagerpage.h"
 
-TCManagerPage::TCManagerPage(QObject *parent) : QObject(parent)
+TCManagerPage::TCManagerPage(QObject *parent)
+    : QObject(parent),
+      TCManager ()
 {
 
 }
+
+
+
+TCManagerPage::TCManagerPage(DataBase *_db)
+    : TCManager (static_cast<DB*>(_db))
+{
+    std::cout << __LINE__ << " " << __FUNCTION__ << " " << "TCMananager DataBase*" << std::endl;
+}
+
+bool TCManagerPage::tcCheck(const QString &mTC)
+{
+        TC item;
+        item.setTCNO (mTC);
+        auto count = this->countItem (item);
+
+        if( count == -1 )
+        {
+            std::cout << "mTC DataBase Count Error: " << __LINE__ << __FUNCTION__ << std::endl;
+            return false;
+        }else if (count == 0 ) {
+            return false;
+        }else{
+            return true;
+        }
+}
+
+TCItem* TCManagerPage::loadByTC( const QString &mTCNO )
+{
+    auto val = this->Load_byTCNO (mTCNO.toStdString ());
+
+    if( val )
+    {
+        return new TCItem(val.value());
+    }else{
+        return nullptr;
+    }
+}
+
+TCItem *TCManagerPage::loadByTel(const QString &mTel)
+{
+    auto val = this->Load_byTEL (mTel.toStdString ());
+
+    if( val )
+    {
+        return new TCItem(val.value());
+    }else{
+        return nullptr;
+    }}
--- TC/tcmanagerpage.h
+++ TC/tcmanagerpage.h
@@ -2,12 +2,23 @@
 #define TCMANAGERPAGE_H
 
 #include <QObject>
+#include "tcmanager.h"
+#include "Comman/database.h"
+#include "tcitem.h"
 
-class TCManagerPage : public QObject
+class TCManagerPage : public QObject , public TCManager
 {
     Q_OBJECT
 public:
     explicit TCManagerPage(QObject *parent = nullptr);
+    TCManagerPage(DataBase* _db );
+
+
+    Q_INVOKABLE bool tcCheck( const QString& mTC );
+
+    Q_INVOKABLE TCItem *loadByTC(const QString &mTCNO);
+
+    Q_INVOKABLE TCItem* loadByTel( const QString &mTel );
 
 signals:
 
