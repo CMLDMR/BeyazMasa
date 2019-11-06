fa678ff8214cef59f3ea1fb78a0ebacf9a7608ef
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-06 16:44:24 +0300

    Kısmı Olarak Güncellendi.

--- Talep/talepitem.cpp
+++ Talep/talepitem.cpp
@@ -4,3 +4,10 @@ TalepItem::TalepItem(QObject *parent) : QObject(parent)
 {
 
 }
+
+TalepItem::TalepItem(const TalepItem &other)
+{
+    this->setDocumentView (other.view ());
+
+//    bsoncxx::types::b_dbpointer ()
+}
--- Talep/talepitem.h
+++ Talep/talepitem.h
@@ -2,16 +2,23 @@
 #define TALEPITEM_H
 
 #include <QObject>
+#include "Talep/talep.h"
 
-class TalepItem : public QObject
+
+
+class TalepItem : public QObject , public Talep
 {
     Q_OBJECT
+    Q_PROPERTY(QString tcOid READ tcOid WRITE setTCOID)
+    Q_PROPERTY(QString mahalle READ mahalle WRITE setMahalle)
 public:
     explicit TalepItem(QObject *parent = nullptr);
+    TalepItem( const TalepItem& other );
 
 signals:
 
 public slots:
 };
+Q_DECLARE_METATYPE(TalepItem)
 
 #endif // TALEPITEM_H
