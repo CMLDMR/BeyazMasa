873e5308e2a48eef388e08d5384a83a346def649
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-07 18:47:51 +0300

    ENUM ve Propertiesler eklendi.

--- Talep/talepitem.cpp
+++ Talep/talepitem.cpp
@@ -1,4 +1,5 @@
 #include "talepitem.h"
+#include <QMetaEnum>
 
 TalepItem::TalepItem(QObject *parent) : QObject(parent)
 {
@@ -8,6 +9,42 @@ TalepItem::TalepItem(QObject *parent) : QObject(parent)
 TalepItem::TalepItem(const TalepItem &other)
 {
     this->setDocumentView (other.view ());
+}
 
-//    bsoncxx::types::b_dbpointer ()
+TalepItem::TalepItem(TalepItem &&other)
+{
+    this->setDocumentView (other.view ());
+}
+
+TalepItem &TalepItem::operator=(const TalepItem &other)
+{
+    this->setDocumentView (other.view ());
+    return *this;
+}
+
+TalepItem &TalepItem::operator=(TalepItem &&other)
+{
+    this->setDocumentView (other.view ());
+    return *this;}
+
+void TalepItem::setTime(const int &mStartOfDay)
+{
+    this->setSec (mStartOfDay);
+}
+
+void TalepItem::setDate(const int &julianDay)
+{
+    this->setJulianDay (julianDay);
+}
+
+void TalepItem::setQMLDurum(const DurumKey &durum)
+{
+    auto durumStr = QMetaEnum::fromType<DurumKey>().valueToKey(durum);
+    this->setDurum (durumStr);
+}
+
+TalepItem::DurumKey TalepItem::durumQML() const
+{
+    auto _durum = QMetaEnum::fromType<DurumKey>().keyToValue (this->durum ().toStdString ().c_str ());
+    return static_cast<DurumKey>(_durum);
 }
--- Talep/talepitem.h
+++ Talep/talepitem.h
@@ -9,15 +9,47 @@
 class TalepItem : public QObject , public Talep
 {
     Q_OBJECT
-    Q_PROPERTY(QString tcOid READ tcOid WRITE setTCOID)
-    Q_PROPERTY(QString mahalle READ mahalle WRITE setMahalle)
+    Q_PROPERTY (QString tcOid READ tcOid WRITE setTCOID)
+    Q_PROPERTY (QString mahalle READ mahalle WRITE setMahalle)
+    Q_PROPERTY (QString adres READ mahalle WRITE setMahalle)
+    Q_PROPERTY (QString konu READ konu WRITE setKonu)
+    Q_PROPERTY (QString saat READ saat)
+    Q_PROPERTY (QString tarih READ tarih)
+    Q_PROPERTY (DurumKey Durum READ durumQML WRITE setQMLDurum )
+    Q_PROPERTY (QString DurumStr READ durum )
+
 public:
     explicit TalepItem(QObject *parent = nullptr);
     TalepItem( const TalepItem& other );
+    TalepItem( TalepItem&& other );
+
+    TalepItem& operator=( const TalepItem& other);
+    TalepItem& operator=( TalepItem&& other );
+
+
+    enum DurumKey
+    {
+        DevamEdiyor,
+        Tamamlandi,
+        TeyitEdilmemis,
+        Beklemede,
+        RedEdildi
+    };
+    Q_ENUM(DurumKey)
+
+
+
+    Q_INVOKABLE void setTime( const int &mStartOfDay );
+    Q_INVOKABLE void setDate( const int &julianDay );
+    Q_INVOKABLE void setQMLDurum( const DurumKey &durum);
+    DurumKey durumQML() const;
+
 
 signals:
 
 public slots:
+
+
 };
 Q_DECLARE_METATYPE(TalepItem)
 
