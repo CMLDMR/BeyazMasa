1cb38a5146e8031e84ee323319f28e9525bb0fa1
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-08 17:24:15 +0300

    Kısmı Olarak Tamamlandı.

--- Talep/talepevent.cpp
+++ Talep/talepevent.cpp
@@ -4,3 +4,25 @@ TalepEvent::TalepEvent(QObject *parent) : QObject(parent)
 {
 
 }
+
+TalepEvent::TalepEvent(const TalepEvent &other)
+{
+    this->setDocumentView (other.view ());
+}
+
+TalepEvent::TalepEvent(TalepEvent &&other)
+{
+    this->setDocumentView (other.view ());
+}
+
+TalepEvent &TalepEvent::operator=(const TalepEvent &other)
+{
+    this->setDocumentView (other.view ());
+    return *this;
+}
+
+TalepEvent &TalepEvent::operator=(TalepEvent &&other)
+{
+    this->setDocumentView (other.view ());
+    return *this;
+}
--- Talep/talepevent.h
+++ Talep/talepevent.h
@@ -2,12 +2,30 @@
 #define TALEPEVENT_H
 
 #include <QObject>
+#include "Talep/talepsubitem.h"
 
-class TalepEvent : public QObject
+class TalepEvent : public QObject , public TalepSubItem
 {
     Q_OBJECT
+
+
 public:
     explicit TalepEvent(QObject *parent = nullptr);
+    TalepEvent( const TalepEvent& other );
+    TalepEvent( TalepEvent&& other );
+
+    TalepEvent& operator=( const TalepEvent& other );
+    TalepEvent& operator=( TalepEvent&& other );
+
+    enum EventType{
+        none = 0,
+        Aciklama,
+        Fotograf,
+        Pdf,
+        Konum,
+        Video
+    };
+    Q_ENUM(EventType)
 
 signals:
 
