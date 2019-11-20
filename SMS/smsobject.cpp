d6b2283c3fa2440aa4ef4bb9578d1ae5369ab7cf
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-20 17:38:42 +0300

    Move ve Copy Constructorlar Eklendi.

--- SMS/smsobject.cpp
+++ SMS/smsobject.cpp
@@ -4,3 +4,25 @@ SMSObject::SMSObject()
 {
 
 }
+
+SMSObject::SMSObject(const SMSObject &other)
+{
+    this->setDocumentView (other.view ());
+}
+
+SMSObject::SMSObject(SMSObject &&other)
+{
+    this->setDocumentView (other.view ());
+}
+
+SMSObject &SMSObject::operator=(const SMSObject &other)
+{
+    this->setDocumentView (other.view ());
+    return *this;
+}
+
+SMSObject &SMSObject::operator=(SMSObject &&other)
+{
+    this->setDocumentView (other.view ());
+    return *this;
+}
--- SMS/smsobject.h
+++ SMS/smsobject.h
@@ -1,11 +1,22 @@
 #ifndef SMSOBJECT_H
 #define SMSOBJECT_H
 
+#include "sms/smsitem.h"
+#include <QObject>
 
-class SMSObject : public SerikBLDCore::SMSItem
+class SMSObject : public QObject , public SerikBLDCore::SMS::SMSItem
 {
+    Q_OBJECT
+    Q_PROPERTY(QString smsText READ smsText WRITE setSMS )
 public:
     SMSObject();
+    SMSObject( const SMSObject& other );
+    SMSObject( SMSObject&& other );
+
+    SMSObject& operator=( const SMSObject& other );
+    SMSObject& operator=( SMSObject&& other );
 };
+
+Q_DECLARE_METATYPE(SMSObject)
 
 #endif // SMSOBJECT_H
