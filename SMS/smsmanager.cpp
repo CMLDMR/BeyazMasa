3cd9a75cc233fc1776bf5fb363853ee531acd988
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-20 17:38:25 +0300

    Move ve Copy Constructorlar Eklendi.

--- SMS/smsmanager.cpp
+++ SMS/smsmanager.cpp
@@ -1,6 +1,43 @@
 #include "smsmanager.h"
 
-SMSManager::SMSManager()
+
+SMSManager::SMSManager(DataBase *_db) : SerikBLDCore::SMSAbstractManager (_db)
 {
 
+}
+
+SMSManager::SMSManager(const DataBase *_db) : SerikBLDCore::SMSAbstractManager (_db)
+{
+
+}
+
+SMSManager::SMSManager(const SMSManager &other) : SerikBLDCore::SMSAbstractManager (other.getDB ())
+{
+
+}
+
+SMSManager::SMSManager(SMSManager &&other) : SerikBLDCore::SMSAbstractManager (other.getDB ())
+{
+
+}
+
+SMSManager &SMSManager::operator=(const SMSManager &other)
+{
+    return *this;
+}
+
+SMSManager &SMSManager::operator=(SMSManager &&other)
+{
+    return other;
+}
+
+
+SerikBLDCore::SMS::SMSItem &SMSManager::insertAndSendSMS(const SerikBLDCore::SMS::SMSItem &item)
+{
+    return SerikBLDCore::SMS::SMSItem().setSMS ("ddd");
+}
+
+void SMSManager::insertSendSMS( const SMSObject *item)
+{
+    std::cout << *item << std::endl;
 }
--- SMS/smsmanager.h
+++ SMS/smsmanager.h
@@ -1,11 +1,31 @@
 #ifndef SMSMANAGER_H
 #define SMSMANAGER_H
 
+#include "sms/smsabstrackmanager.h"
+#include "Comman/database.h"
+#include "Comman/managerpage.h"
+#include <QObject>
+#include "smsobject.h"
 
-class SMSManager : public SerikBLDCore::SMS::SMSAbstractManager
+class SMSManager : public QObject , public SerikBLDCore::SMSAbstractManager
 {
+    Q_OBJECT
 public:
-    SMSManager();
+//    explicit SMSManager(QObject *parent = nullptr);
+    SMSManager(DataBase *_db = nullptr );
+    SMSManager(const DataBase *_db );
+    SMSManager( const SMSManager& other );
+    SMSManager( SMSManager&& other );
+
+    SMSManager& operator=( const SMSManager& other );
+    SMSManager& operator=( SMSManager&& other );
+
+
+    SerikBLDCore::SMS::SMSItem & insertAndSendSMS(const SerikBLDCore::SMS::SMSItem &item) override;
+
+    Q_INVOKABLE void insertSendSMS(const SMSObject *item );
 };
+
+Q_DECLARE_METATYPE(SMSManager)
 
 #endif // SMSMANAGER_H
