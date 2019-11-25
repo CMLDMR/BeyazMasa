74d0561f78f9e6c2ade8828f0a6e2eeb2ab0722f
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-25 17:35:03 +0300

    Tamamlandı.

--- BilgiEdinme/bilgiedinmemanagerpage.cpp
+++ BilgiEdinme/bilgiedinmemanagerpage.cpp
@@ -1,6 +1,24 @@
 #include "bilgiedinmemanagerpage.h"
 
 BilgiEdinmeManagerPage::BilgiEdinmeManagerPage()
+    :SerikBLDCore::BilgiEdinme::BilgiEdinmeManager (new DB())
 {
+
+}
+
+BilgiEdinmeManagerPage::BilgiEdinmeManagerPage(DB *db)
+    :SerikBLDCore::BilgiEdinme::BilgiEdinmeManager (db)
+{
+
+
+   auto cursor = this->UpdateList (SerikBLDCore::BilgiEdinme::BilgiEdinmeItem());
+
+
+   for( auto item : cursor )
+   {
+       BilgiEdinmeValue item_;
+       item_.setDocumentView (item.view ());
+       this->insertModel (item_);
+   }
 
 }
--- BilgiEdinme/bilgiedinmemanagerpage.h
+++ BilgiEdinme/bilgiedinmemanagerpage.h
@@ -1,11 +1,22 @@
 #ifndef BILGIEDINMEMANAGERPAGE_H
 #define BILGIEDINMEMANAGERPAGE_H
 
+#include "bilgiEdinme/bilgiedinmemanager.h"
+#include "Comman/managerpage.h"
+#include "BilgiEdinme/bilgiedinmevalue.h"
 
-class BilgiEdinmeManagerPage : public BilgiEdinmeManager
+class BilgiEdinmeManagerPage : public ManagerPage<BilgiEdinmeValue> , public SerikBLDCore::BilgiEdinme::BilgiEdinmeManager
 {
+    Q_OBJECT
+    Q_PROPERTY(QQmlListProperty<BilgiEdinmeValue> list READ list NOTIFY listChanged )
+
 public:
-    BilgiEdinmeManagerPage();
+    explicit BilgiEdinmeManagerPage();
+    BilgiEdinmeManagerPage(DB* db);
+
+private:
+
 };
+
 
 #endif // BILGIEDINMEMANAGERPAGE_H
