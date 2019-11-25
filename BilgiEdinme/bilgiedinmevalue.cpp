f52b00339d7b73170687f1eac48c0c362223d177
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-25 17:35:13 +0300

    Kısmi Tamamlandı.

--- BilgiEdinme/bilgiedinmevalue.cpp
+++ BilgiEdinme/bilgiedinmevalue.cpp
@@ -1,6 +1,41 @@
 #include "bilgiedinmevalue.h"
 
 BilgiEdinmeValue::BilgiEdinmeValue()
+    :SerikBLDCore::BilgiEdinme::BilgiEdinmeItem (nullptr)
 {
 
+}
+
+BilgiEdinmeValue::BilgiEdinmeValue(const BilgiEdinmeValue &other)
+{
+    this->setDocumentView (other.view ());
+}
+
+BilgiEdinmeValue::BilgiEdinmeValue(BilgiEdinmeValue &&other)
+{
+    this->setDocumentView (other.view ());
+}
+
+BilgiEdinmeValue::BilgiEdinmeValue(const SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &other)
+{
+    this->setDocumentView (other.view ());
+}
+
+BilgiEdinmeValue::BilgiEdinmeValue(SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &&other)
+{
+    this->setDocumentView (other.view ());
+}
+
+
+
+BilgiEdinmeValue &BilgiEdinmeValue::operator=(const SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &other)
+{
+    this->setDocumentView (other.view ());
+    return *this;
+}
+
+BilgiEdinmeValue &BilgiEdinmeValue::operator=(SerikBLDCore::BilgiEdinme::BilgiEdinmeItem &&other)
+{
+    this->setDocumentView (other.view ());
+    return *this;
 }
--- BilgiEdinme/bilgiedinmevalue.h
+++ BilgiEdinme/bilgiedinmevalue.h
@@ -1,11 +1,30 @@
 #ifndef BILGIEDINMEVALUE_H
 #define BILGIEDINMEVALUE_H
 
+#include <QObject>
+#include "bilgiEdinme/bilgiedinmeitem.h"
+
 
 class BilgiEdinmeValue : public QObject , public SerikBLDCore::BilgiEdinme::BilgiEdinmeItem
 {
+    Q_OBJECT
+    Q_PROPERTY (QString konu  READ konu WRITE setKonu)
+    Q_PROPERTY (QString tarih READ tarih )
+    Q_PROPERTY (QString saat  READ saat )
+    Q_PROPERTY (QString birim READ birim )
 public:
     BilgiEdinmeValue();
+    BilgiEdinmeValue( const BilgiEdinmeValue& other );
+    BilgiEdinmeValue( BilgiEdinmeValue&& other );
+    BilgiEdinmeValue( const SerikBLDCore::BilgiEdinme::BilgiEdinmeItem& other );
+    BilgiEdinmeValue( SerikBLDCore::BilgiEdinme::BilgiEdinmeItem&& other );
+
+
+
+    BilgiEdinmeValue& operator=( const SerikBLDCore::BilgiEdinme::BilgiEdinmeItem& other );
+    BilgiEdinmeValue& operator=( SerikBLDCore::BilgiEdinme::BilgiEdinmeItem&& other );
 };
+
+Q_DECLARE_METATYPE(BilgiEdinmeValue)
 
 #endif // BILGIEDINMEVALUE_H
