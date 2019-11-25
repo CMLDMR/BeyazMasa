ed08d4f735dddbd6602760e241828b40a1d5f58b
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-25 17:37:27 +0300

    Oluşturuldu.

--- ScriptFiles/MainMenuScript.js
+++ ScriptFiles/MainMenuScript.js
@@ -1,3 +1,11 @@
-function func() {
-
+function loadBilgiEdinme() {
+    var component = Qt.createComponent("qrc:/BilgiEdinme/BilgiEdinmeMainPage.qml");
+    if (component.status === Component.Ready) {
+        var sprite = component.createObject(appWindow);
+        if (sprite === null) {
+            Backend.message = "Bilgi Edinme Nesnesi Oluşturulamadı";
+        }
+    } else if (component.status === Component.Error) {
+        Backend.message = "Bilgi Edinme Sayfası Hata: " + component.errorString();
+    }
 }
