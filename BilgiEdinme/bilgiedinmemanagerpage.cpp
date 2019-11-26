87ded74e7595e5d194b16d0be79beee866712213
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-26 16:20:29 +0300

    Liste Güncelleme Tamamlandı.

--- BilgiEdinme/bilgiedinmemanagerpage.cpp
+++ BilgiEdinme/bilgiedinmemanagerpage.cpp
@@ -9,16 +9,36 @@ BilgiEdinmeManagerPage::BilgiEdinmeManagerPage()
 BilgiEdinmeManagerPage::BilgiEdinmeManagerPage(DB *db)
     :SerikBLDCore::BilgiEdinme::BilgiEdinmeManager (db)
 {
+    this->refreshList ();
+}
 
+BilgiEdinmeValue *BilgiEdinmeManagerPage::valueAt(const QString &byOid)
+{
+    return new BilgiEdinmeValue(this->itemAt (byOid));
+}
 
-   auto cursor = this->UpdateList (SerikBLDCore::BilgiEdinme::BilgiEdinmeItem());
+bool BilgiEdinmeManagerPage::updateItem(BilgiEdinmeValue *item)
+{
+    if( this->UpdateItem (*item) ){
+        _refreshList ();
+        return true;
+    }
+    return false;
+}
 
+void BilgiEdinmeManagerPage::refreshList()
+{
+    _refreshList();
+}
 
-   for( auto item : cursor )
-   {
-       BilgiEdinmeValue item_;
-       item_.setDocumentView (item.view ());
-       this->insertModel (item_);
-   }
-
+void BilgiEdinmeManagerPage::_refreshList()
+{
+    auto cursor = this->UpdateList (SerikBLDCore::BilgiEdinme::BilgiEdinmeItem());
+    this->clearModel ();
+    for( auto item : cursor )
+    {
+        BilgiEdinmeValue item_;
+        item_.setDocumentView (item.view ());
+        this->insertModel (item_);
+    }
 }
