3c050a3906752fde46bb3cad3f980e995a22b841
Cemal DEMİR <cemaldemir07@gmail.com>
2020-01-06 09:31:41 +0300

    TCManagerPage Yükleme Function Eklendi ve Tanımlandı.

--- ScriptFiles/MainMenuScript.js
+++ ScriptFiles/MainMenuScript.js
@@ -9,3 +9,16 @@ function loadBilgiEdinme() {
         Backend.message = "Bilgi Edinme Sayfası Hata: " + component.errorString();
     }
 }
+
+
+function loadTCVeriTabani() {
+    var component = Qt.createComponent("qrc:/TC/TCManagerPage.qml");
+    if (component.status === Component.Ready) {
+        var sprite = component.createObject(appWindow);
+        if (sprite === null) {
+            Backend.message = "TC Veritabanı Sayfası Oluşturulamadı";
+        }
+    } else if (component.status === Component.Error) {
+        Backend.message = "TC Veritabanı Sayfası Hata: " + component.errorString();
+    }
+}
